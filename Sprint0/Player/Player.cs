34db0627e01f20f8e4b5d1173c71436700514c13
Michael <57294690+mfong121@users.noreply.github.com>
2021-09-18 18:50:27 -0400

    Added position variables to Player class

--- Sprint0/Player/Player.cs
+++ Sprint0/Player/Player.cs
@@ -2,9 +2,11 @@ namespace Player
 {
     public class Player{
         PlayerState playerState;
+        int xPos;
+        int yPos;
         public Player()
         {
-
+            //initialize xPos and yPos. Probably need to initialize playerState too. Maybe init playerState to idle facing down
         }
     }
 }
