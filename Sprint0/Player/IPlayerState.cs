36c428fab3aebe8b6fa8e7e01f32a536dbb90df1
Michael <57294690+mfong121@users.noreply.github.com>
2021-09-18 18:48:20 -0400

    Added comments to IPlayerState

--- Sprint0/Player/IPlayerState.cs
+++ Sprint0/Player/IPlayerState.cs
@@ -1,10 +1,12 @@
 using System;
 namespace Player
 {
-	interface IPlayerState
+	interface IPlayerState //Trying to implement using state pattern, NOT state machine
+						   //http://web.cse.ohio-state.edu/~boggus.2/3902/slides/GoombaStateExample.cs
 	{
 		void ChangeDirection();
 		void ChangeWalkingState(); // Meant to switch to/from "idling" (standing still) and walking
 		void Update();
+		//Damage should be implemented as a decorator on the player, not the state (look at notes on cse3902 homepage)
 	}
 }
