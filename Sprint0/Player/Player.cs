e94557439658994137438a3e61a71a3126deeed0
Michael <57294690+mfong121@users.noreply.github.com>
2021-09-18 21:06:47 -0400

    Revert "Added position variables to Player class"
    This reverts commit 34db0627e01f20f8e4b5d1173c71436700514c13.

--- Sprint0/Player/Player.cs
+++ Sprint0/Player/Player.cs
@@ -2,11 +2,9 @@ namespace Player
 {
     public class Player{
         PlayerState playerState;
-        int xPos;
-        int yPos;
         public Player()
         {
-            //initialize xPos and yPos. Probably need to initialize playerState too. Maybe init playerState to idle facing down
+
         }
     }
 }
