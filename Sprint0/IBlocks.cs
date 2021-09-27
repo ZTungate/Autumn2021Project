946f40982465ce14f3e4208da537ae9a7c6a7d52
ZTungate <54688105+ZTungate@users.noreply.github.com>
2021-09-27 14:15:04 -0400

    Add files via upload
    These files wouldn't push so adding them via upload

---
+++ Sprint0/IBlocks.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Sprint0.Blocks
+{
+    public interface IBlocks
+    {
+        Texture2D Texture { get; set; }
+
+        Rectangle[] sourceRect { get; set; }
+
+        Vector2 Position { get; set; }
+
+        void Update(GameTime gameTime);
+
+        void Draw(SpriteBatch spriteBatch);
+
+    }
+}
