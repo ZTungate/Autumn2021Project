a03ce11c76bcba9a522409253825d8a0c69fddc8
mfong121 <michael.fong121@gmail.com>
2021-09-29 16:58:24 -0400

    Created RightIdleState for player, added Position to player.cs

---
+++ Sprint0/Player/RightIdleState.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Player
+{
+	public class RightIdleState : IPlayerState
+	{
+		public RightIdleState()
+		{
+		}
+
+        public void ChangeDirection()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ChangeWalkingState()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
