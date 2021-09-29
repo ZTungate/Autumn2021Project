683ae96963f6cd1c59746c1c0d4103c779245b23
BuildTools <unconfigured@null.spigotmc.org>
2021-09-29 16:54:07 -0400

    started making link sprites

---
+++ Sprint0/Player/LinkSpriteFactory.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sprint0
+{
+    public class LinkSpriteFactory
+    {
+        private Texture2D linkSpriteSheet;
+
+        private static LinkSpriteFactory instance = new LinkSpriteFactory();
+
+        public static LinkSpriteFactory Instance
+        {
+            get
+            {
+                return instance;
+            }
+        }
+
+        private LinkSpriteFactory()
+        {
+        }
+
+        public void LoadAllTextures(ContentManager content)
+        {
+            linkSpriteSheet = content.Load<Texture2D>("LinkSpriteSheet");
+        }
+
+        //TODO Add CreateSprite methods for each enemy type
+        public ISprite CreateSkeletonSprite()
+        {
+            return new SkeletonSprite(linkSpriteSheet);
+        }
+
+        public ISprite CreateSlimeSprite()
+        {
+            return new SlimeSprite(linkSpriteSheet);
+        }
+    }
+}
---
+++ Sprint0/Sprites/Link Sprites/RightIdleLinkSprite.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sprint0
+{
+    public class RightIdleLinkSprite : IAnimatedSprite
+    {
+        public float Timer { get; set; } = 0f;
+        public float Interval { get; set; } = 40f;
+        public int CurrentFrame { get; set; } = 0;
+        public int FrameCount { get; set; } = 2;
+        public float SpriteSpeed { get; set; } = 0;
+        public Texture2D Texture { get; set; }
+        public Rectangle[] SourceRect { get; set; }
+        public Vector2 Position { get; set; }
+
+        public RightIdleLinkSprite(Texture2D spriteSheet)
+        {
+            //Set the texture2D to the provided spriteSheet (already initialized by factory)
+            Texture = spriteSheet;
+            SourceRect = new Rectangle[2];
+
+            //Set the two frames for the skeleton animation
+            SourceRect[0] = new Rectangle(1, 11, 16, 16);
+            SourceRect[1] = new Rectangle(1, 11, 16, 16);
+
+            //Dummy position, needs to be fixed by adding pos relevant to the enemy. TODO fix
+            Position = new Vector2(100,100);
+        }
+
+        public void Update(GameTime gameTime)
+        {
+
+        }
+
+
+    }
+}
