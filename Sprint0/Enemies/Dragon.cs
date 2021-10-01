89b8ee68ed5b9a03cfa7de8fde61e8390a576773
Jonah Silsdorf <silsdorf.1@buckeyemail.osu.edu>
2021-10-01 17:48:05 -0400

    Implemented dragon movement, moved old enemy interfaces to OldEnemies

--- Sprint0/Enemies/Dragon.cs
+++ Sprint0/Enemies/Dragon.cs
@@ -12,6 +12,10 @@ namespace Sprint0.Enemies
     {
         //ISprite for the enemy
         ISprite mySprite;
+
+        //Timers for updating sprite without moving
+        private int interval = 120;
+        private int timer = 0;
         public ISprite Sprite
         {
             //Allow sprite to be set by the spriteFactory, and return mySprite when requested.
@@ -29,10 +33,38 @@ namespace Sprint0.Enemies
         {
             //Update the sprite
             mySprite.Update(gameTime);
+            //Timer to prevent from moving too fast, should unify with the timer in sprite.Update();
+            if (timer < interval)
+            {
+                timer += gameTime.ElapsedGameTime.Milliseconds;
+            }
+            else
+            {
+                timer = 0;
+                mySprite.Position = DragonMove();
+            }
         }
         public Dragon()
         {
             //Nothing special about the dragon object itself, no implementation required
+        }
+
+        public Vector2 DragonMove()
+        {
+            Random rand = new Random();
+            Vector2 pos = mySprite.Position;
+            int val = rand.Next(3);
+
+            if (val == 0)
+            {
+                pos.X += 5;
+            }
+            else if (val == 1)
+            {
+                pos.X -= 5;
+            }
+                return pos;
+            
         }
     }
 }
