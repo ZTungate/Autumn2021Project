3bec0c670b165c45424e2deb05f1507809918af0
mfong121 <michael.fong121@gmail.com>
2021-10-04 23:21:44 -0400

    Fixed a warning with "using Sprint2.enemies"

--- Sprint0/Main/Game1.cs
+++ Sprint0/Main/Game1.cs
@@ -1,9 +1,7 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
-using Microsoft.Xna.Framework.Input;
 using Sprint2.Enemies;
 using Sprint2.Player;
-using Sprint2.Enemies;
 using Sprint2.Items;
 using Sprint2.Blocks;
 using System.Collections.Generic;
