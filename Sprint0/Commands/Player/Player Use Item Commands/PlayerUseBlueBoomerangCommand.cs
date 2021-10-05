0f926f10628185f42d1dc6a1aa2500145521aeab
BuildTools <unconfigured@null.spigotmc.org>
2021-10-04 23:15:12 -0400

    fixed player using items mwah

--- Sprint0/Commands/Player/Player Use Item Commands/PlayerUseBlueBoomerangCommand.cs
+++ Sprint0/Commands/Player/Player Use Item Commands/PlayerUseBlueBoomerangCommand.cs
@@ -15,7 +15,26 @@ namespace Sprint2.Commands
         public void Execute()
         {
 
+            switch (game.link.facing) {
+                case Player.direction.up:
+                    game.link.sprite = game.linkSpriteFactory.UpUseItemLinkSprite(game.link);
 
+                    break;
+                case Player.direction.down:
+                    game.link.sprite = game.linkSpriteFactory.DownUseItemLinkSprite(game.link);
+
+                    break;
+                case Player.direction.left:
+                    game.link.sprite = game.linkSpriteFactory.LeftUseItemLinkSprite(game.link);
+
+                    break;
+                case Player.direction.right:
+                    game.link.sprite = game.linkSpriteFactory.RightUseItemLinkSprite(game.link);
+
+                    break;
+                default:
+                    break;
+            }
             game.link.BlueBoomerangAttack();
             
         }
