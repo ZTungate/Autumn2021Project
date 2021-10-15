4960281b49dfce20a628b1d46f91d42bebd02850
Jonah Silsdorf <silsdorf.1@buckeyemail.osu.edu>
2021-10-15 17:19:39 -0400

    Created RightIdleLinkState for eventual state pattern setup for link

---
+++ Sprint0/Player/States/RightIdleLinkState.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+
+namespace Sprint2.Player
+{
+	public class RightIdleLinkState : ILinkState
+	{
+        private ILink link;
+        private ISprite sprite;
+        public RightIdleLinkState(ILink Link, ISprite sprite)
+		{
+            this.link = Link;
+            this.sprite = sprite;
+        }
+
+        public void takeDamage()
+        {
+            //Call on link to take damage. Does this need to be here? Might not be necesary in the state itself.
+            link.takeDamage();
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            //What needs to be updated in the State?
+
+        }
+
+        public void useItem(Game1 game)
+        {
+            game.link.sprite = game.linkSpriteFactory.RightUseItemLinkSprite(game.link);
+        }
+    }
+}
