56f0764e3b7ba3193734d34d049e1b931cb99a1b
Zach <Tungate.4@osu.edu>
2021-10-21 17:12:12 -0400

    Finished implementing most collisions

--- Sprint0/Collisions/CollisionDetection.cs
+++ Sprint0/Collisions/CollisionDetection.cs
@@ -7,6 +7,7 @@ using Sprint2.Blocks;
 using Sprint2.Enemies;
 using Sprint2.Items;
 using Sprint2.Player;
+using Sprint2.Projectiles;
 
 namespace Sprint0.Collisions
 {
@@ -14,7 +15,7 @@ namespace Sprint0.Collisions
     {
 
 
-
+        //heavilly overloaded to check collisions
         public ICollision detectCollision(IBlocks object1, IBlocks object2)
         {
             Rectangle one = object1.destRect;
@@ -157,13 +158,185 @@ namespace Sprint0.Collisions
             return collision;
         }
 
-        public ICollision detectCollision(ILink object1, ISprite object2, ArrowItem test) //Collisions between enemies similar logic different set up due to differences
+        public ICollision detectCollision(ILink object1, AbstractItem object2) //Collisions between enemies similar logic different set up due to differences
+        {
+            Vector2 holder1 = object1.position;
+            
+            
+            Rectangle one = new Rectangle((int)holder1.X, (int)holder1.Y, object1.sprite.SourceRect[object1.sprite.CurrentFrame].Width * 2, object1.sprite.SourceRect[object1.sprite.CurrentFrame].Height * 2);
+            Rectangle two = object2.GetRectangle();
+            Rectangle Overlap = Rectangle.Intersect(one, two);
+            ColDirections location; //placeholder
+            if (Overlap.Width <= Overlap.Height) //this would mean left-right collision
+            {
+                int check = one.X - two.X;
+                if (check < 0) location = ColDirections.East; //right collision
+
+                else location = ColDirections.West; //left collision
+
+            }
+            else //this means top-bottom collision 
+            {
+                int check = one.Y - two.Y;
+                if (check < 0) location = ColDirections.South; //Bottom collision
+
+                else location = ColDirections.North; //Top collision
+            }
+            ICollision collision = new GenCollision(location, one.Intersects(two));
+
+            return collision;
+        }
+
+        public ICollision detectCollision(AbstractItem object1, AbstractItem object2) //Collisions between enemies similar logic different set up due to differences
+        {
+
+
+
+            Rectangle one = object1.GetRectangle();
+            Rectangle two = object2.GetRectangle();
+            Rectangle Overlap = Rectangle.Intersect(one, two);
+            ColDirections location; //placeholder
+            if (Overlap.Width <= Overlap.Height) //this would mean left-right collision
+            {
+                int check = one.X - two.X;
+                if (check < 0) location = ColDirections.East; //right collision
+
+                else location = ColDirections.West; //left collision
+
+            }
+            else //this means top-bottom collision 
+            {
+                int check = one.Y - two.Y;
+                if (check < 0) location = ColDirections.South; //Bottom collision
+
+                else location = ColDirections.North; //Top collision
+            }
+            ICollision collision = new GenCollision(location, one.Intersects(two));
+
+            return collision;
+        }
+
+        public ICollision detectCollision(AbstractItem object1, IEnemy object2) //Collisions between enemies similar logic different set up due to differences
+        {
+            Vector2 holder2 = object2.Position;
+
+
+            Rectangle one = object1.GetRectangle();
+            Rectangle two = new Rectangle((int)holder2.X, (int)holder2.Y, object2.Sprite.SourceRect[object2.Sprite.CurrentFrame].Width * 2, object2.Sprite.SourceRect[object2.Sprite.CurrentFrame].Height * 2);
+            Rectangle Overlap = Rectangle.Intersect(one, two);
+            ColDirections location; //placeholder
+            if (Overlap.Width <= Overlap.Height) //this would mean left-right collision
+            {
+                int check = one.X - two.X;
+                if (check < 0) location = ColDirections.East; //right collision
+
+                else location = ColDirections.West; //left collision
+
+            }
+            else //this means top-bottom collision 
+            {
+                int check = one.Y - two.Y;
+                if (check < 0) location = ColDirections.South; //Bottom collision
+
+                else location = ColDirections.North; //Top collision
+            }
+            ICollision collision = new GenCollision(location, one.Intersects(two));
+
+            return collision;
+        }
+
+        public ICollision detectCollision(IProjectile object1, IProjectile object2) //Collisions between enemies similar logic different set up due to differences
+        {
+            Vector2 holder1 = object1.Position;
+            Vector2 holder2 = object2.Position;
+
+            Rectangle one = new Rectangle((int)holder1.X, (int)holder1.Y, object1.Sprite.SourceRect[object1.Sprite.CurrentFrame].Width * 2, object1.Sprite.SourceRect[object1.Sprite.CurrentFrame].Height * 2);
+            Rectangle two = new Rectangle((int)holder2.X, (int)holder2.Y, object2.Sprite.SourceRect[object2.Sprite.CurrentFrame].Width * 2, object2.Sprite.SourceRect[object2.Sprite.CurrentFrame].Height * 2);
+            Rectangle Overlap = Rectangle.Intersect(one, two);
+            ColDirections location; //placeholder
+            if (Overlap.Width <= Overlap.Height) //this would mean left-right collision
+            {
+                int check = one.X - two.X;
+                if (check < 0) location = ColDirections.East; //right collision
+
+                else location = ColDirections.West; //left collision
+
+            }
+            else //this means top-bottom collision 
+            {
+                int check = one.Y - two.Y;
+                if (check < 0) location = ColDirections.South; //Bottom collision
+
+                else location = ColDirections.North; //Top collision
+            }
+            ICollision collision = new GenCollision(location, one.Intersects(two));
+
+            return collision;
+        }
+        public ICollision detectCollision(ILink object1, IProjectile object2) //Collisions between enemies similar logic different set up due to differences
         {
             Vector2 holder1 = object1.position;
             Vector2 holder2 = object2.Position;
-            
+
             Rectangle one = new Rectangle((int)holder1.X, (int)holder1.Y, object1.sprite.SourceRect[object1.sprite.CurrentFrame].Width * 2, object1.sprite.SourceRect[object1.sprite.CurrentFrame].Height * 2);
-            Rectangle two = new Rectangle((int)holder2.X, (int)holder2.Y, object2.SourceRect[object2.CurrentFrame].Width * 2, object2.SourceRect[object2.CurrentFrame].Height * 2);
+            Rectangle two = new Rectangle((int)holder2.X, (int)holder2.Y, object2.Sprite.SourceRect[object2.Sprite.CurrentFrame].Width * 2, object2.Sprite.SourceRect[object2.Sprite.CurrentFrame].Height * 2);
+            Rectangle Overlap = Rectangle.Intersect(one, two);
+            ColDirections location; //placeholder
+            if (Overlap.Width <= Overlap.Height) //this would mean left-right collision
+            {
+                int check = one.X - two.X;
+                if (check < 0) location = ColDirections.East; //right collision
+
+                else location = ColDirections.West; //left collision
+
+            }
+            else //this means top-bottom collision 
+            {
+                int check = one.Y - two.Y;
+                if (check < 0) location = ColDirections.South; //Bottom collision
+
+                else location = ColDirections.North; //Top collision
+            }
+            ICollision collision = new GenCollision(location, one.Intersects(two));
+
+            return collision;
+        }
+
+        public ICollision detectCollision(IBlocks object1, IProjectile object2) //Collisions between enemies similar logic different set up due to differences
+        {
+            
+            Vector2 holder2 = object2.Position;
+
+            Rectangle one = object1.destRect; 
+            Rectangle two = new Rectangle((int)holder2.X, (int)holder2.Y, object2.Sprite.SourceRect[object2.Sprite.CurrentFrame].Width * 2, object2.Sprite.SourceRect[object2.Sprite.CurrentFrame].Height * 2);
+            Rectangle Overlap = Rectangle.Intersect(one, two);
+            ColDirections location; //placeholder
+            if (Overlap.Width <= Overlap.Height) //this would mean left-right collision
+            {
+                int check = one.X - two.X;
+                if (check < 0) location = ColDirections.East; //right collision
+
+                else location = ColDirections.West; //left collision
+
+            }
+            else //this means top-bottom collision 
+            {
+                int check = one.Y - two.Y;
+                if (check < 0) location = ColDirections.South; //Bottom collision
+
+                else location = ColDirections.North; //Top collision
+            }
+            ICollision collision = new GenCollision(location, one.Intersects(two));
+
+            return collision;
+        }
+        public ICollision detectCollision(IProjectile object1, IEnemy object2) //Collisions between enemies similar logic different set up due to differences
+        {
+            Vector2 holder1 = object1.Position;
+            Vector2 holder2 = object2.Position;
+
+            Rectangle one = new Rectangle((int)holder1.X, (int)holder1.Y, object1.Sprite.SourceRect[object1.Sprite.CurrentFrame].Width * 2, object1.Sprite.SourceRect[object1.Sprite.CurrentFrame].Height * 2);
+            Rectangle two = new Rectangle((int)holder2.X, (int)holder2.Y, object2.Sprite.SourceRect[object2.Sprite.CurrentFrame].Width * 2, object2.Sprite.SourceRect[object2.Sprite.CurrentFrame].Height * 2);
             Rectangle Overlap = Rectangle.Intersect(one, two);
             ColDirections location; //placeholder
             if (Overlap.Width <= Overlap.Height) //this would mean left-right collision
