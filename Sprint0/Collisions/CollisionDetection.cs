343aace595d8438f123e91b2c34e32bfbb747255
Zach <Tungate.4@osu.edu>
2021-10-21 00:19:35 -0400

    Block Collisions with blocks should work now, useless for now but hey its there

--- Sprint0/Collisions/CollisionDetection.cs
+++ Sprint0/Collisions/CollisionDetection.cs
@@ -7,23 +7,35 @@ using Sprint2.Blocks;
 
 namespace Sprint0.Collisions
 {
-    class CollisionDetection : ICollision
+    class CollisionDetection
     {
 
-        bool Collision;
-
-        ColDirections side;
-        public bool IsCollision { get => Collision; set => Collision = value; }
-
-
-        public ColDirections direction { get => side; set => side = value; }
 
 
         public ICollision detectCollision(IBlocks object1, IBlocks object2)
         {
+            Rectangle one = object1.destRect;
+            Rectangle two = object2.destRect;
+            Rectangle Overlap = Rectangle.Intersect(one, two);
+            ColDirections location = ColDirections.North; //placeholder
+            if(Overlap.Width <= Overlap.Height) //this would mean left-right collision
+            {
+                int check = one.X - two.X;
+                if (check < 0) location = ColDirections.East; //right collision
+                
+                else location = ColDirections.West; //left collision
+
+            }
+            else //this means top-bottom collision 
+            {
+                int check = one.Y - two.Y;
+                if (check < 0) location = ColDirections.South; //Bottom collision
+
+                else location = ColDirections.North; //Top collision
+            }
+            ICollision collision = new GenCollision(location, one.Intersects(two));
             
-            
-            return null;
+            return collision;
         }
     }
 }
---
+++ Sprint0/Collisions/GenCollision.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sprint0.Collisions
+{
+    class GenCollision : ICollision
+    {
+        public bool IsCollision { get; set; }
+        public ColDirections direction { get; set; }
+
+        public GenCollision(ColDirections direction, bool IsCollision)
+        {
+
+        }
+    }
+}
