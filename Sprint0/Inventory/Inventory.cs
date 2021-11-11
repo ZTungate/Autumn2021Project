3f5f17e26036f033655378ce5e51fb7af1f2ecf6
Zach Tungate <tungate.4@buckeyemail.osu.edu>
2021-11-11 13:01:49 -0500

    Inventory class

---
+++ Sprint0/Inventory/Inventory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sprint0.Inventory
+{
+    public class Inventory
+    {
+        protected int arrows = 0;
+        protected int bombs = 0;
+        
+        public Inventory()
+        {
+
+        }
+    }
+}
