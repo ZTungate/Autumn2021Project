9b4260e9edde1aed3580eae4738e3ce751e19eb3
JSilsdorf <90729521+JSilsdorf@users.noreply.github.com>
2021-11-15 22:36:44 -0500

    Create Code Readability - ProjCollision.cs

---
+++ Code Reviews/Sprint4/Code Readability - ProjCollision.cs
@@ -0,0 +1,13 @@
+Author of the code review: Jonah Silsdorf
+Date of the code review: 11/15/21
+Sprint number: 4
+Name of the .cs file being reviewed:  ProjCollision.cs
+Author of the .cs file being reviewed: Michael
+Number of minutes taken to complete the review: 15
+
+This file has decent readability, but has room for improvement.
+Variable names are pretty clear.
+Method names are a little confusing with the abbreviation, but with context can be understood.
+No comments are present but each class in this file is fairly short.
+There are some naming rule violations in every class for the property names.
+
