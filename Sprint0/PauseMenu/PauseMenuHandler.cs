351520feb464b7bef7663b63f0d89137cffe5a8a
Zach Tungate <tungate.4@buckeyemail.osu.edu>
2021-12-05 13:34:24 -0500

    Added the class for the start of pause menu working on fully implemented still doing back end for it

---
+++ Sprint0/PauseMenu/PauseMenuHandler.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Poggus.PauseMenu
+{
+    class PauseMenuHandler
+    {
+
+
+
+        public PauseMenuHandler(Game1 game)
+        {
+
+        }
+    }
+}
