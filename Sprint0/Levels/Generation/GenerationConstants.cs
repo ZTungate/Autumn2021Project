39c72cc63ea180ac33938d87ee47638eb94657b1
Setlock <42411693+Setlock@users.noreply.github.com>
2021-12-06 21:15:43 -0500

    Improved procedural dungeons, added collisions between moveable blocks and enemies, added item test level

--- Sprint0/Levels/Generation/GenerationConstants.cs
+++ Sprint0/Levels/Generation/GenerationConstants.cs
@@ -9,8 +9,8 @@ namespace Poggus.Levels.Generation
         public static float featureSize = 5f;
         public static int blockScaleX = 16, blockScaleY = 16;
         //Chance = (1 / value)
-        public static int itemSpawnChance = 75, enemySpawnChance = 50;
+        public static int itemSpawnChance = 75, enemySpawnChance = 35;
         public static int bombChance = 25, mapChance = 20, keyChance = 20, arrowChance = 5, fairyChance = 50,heartChance = 10,heartContainerChance = 15;
-        public static int slimeChance = 5, batChance = 5, skeletonChance = 8, throwerChance = 10, bladeChance = 20;
+        public static int slimeChance = 5, batChance = 5, skeletonChance = 4, throwerChance = 3, bladeChance = 5;
     }
 }
